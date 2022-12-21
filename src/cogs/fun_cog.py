b83e0f4d61aa355f5090341ea8de93fb1970d7ba
Anthony Huang <Huang.Anthony@outlook.com>
2022-12-21 00:15:32 -0500

    Added fun cog and minor adjustments
    + Added fun cog commands
    + Added logging to fun cog and help cog
    + Added on_ready method to fun cog and help cog
    + Updated help command description

---
+++ src/cogs/fun_cog.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import tasks, commands
+import textwrap
+import random
+
+from src.discord_bot import DiscordBot
+from src.utils.logger import setup_logger
+
+class FunCog(commands.Cog):
+    def __init__(self, bot: DiscordBot):
+        self._bot = bot
+        self._logger = setup_logger('bot.cog.fun', '/data/discord.log')
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self._logger.info("fun cog is ready.")
+
+    @commands.hybrid_command(name="coinflip", description="Does a coin flip.")
+    async def coinflip(self, ctx: commands.context.Context):
+        flip = random.randint(1,2)
+        await ctx.reply("Heads." if flip == 1 else "Tails.")
+
+    @commands.hybrid_command(name="random", description="Choose a number between 1 to max number.")
+    async def random(self, ctx: commands.context.Context, number:int = commands.parameter(default=100, description="The max random number.")):
+        if (number <= 1 or number > 999999):
+            await ctx.reply("Choose a number higher than 1, but lower or equals to 999999")
+            return
+        result = random.randint(1, number)
+        await ctx.reply(f"{result} out of {number}.")
+
+    @commands.hybrid_command(name="should", description="Ask the bot for random answers.")
+    async def should(self, ctx: commands.context.Context, *, description = commands.parameter(default=None, description="Question.")):
+        if (not description):
+            await ctx.reply("Add a description.")
+            return
+
+        result = random.randint(1, 10)
+
+        if (result == 9 or result == 10):
+            message = "Maybe."
+        elif (result <= 8 or result >= 5):
+            message = "Yes."
+        else:
+            message = "No."
+
+        await ctx.reply(message)
+
+async def setup(bot: DiscordBot):
+    await bot.add_cog(FunCog(bot=bot))
--- src/cogs/help_cog.py
+++ src/cogs/help_cog.py
@@ -3,10 +3,16 @@ from discord.ext import tasks, commands
 import textwrap
 
 from src.discord_bot import DiscordBot
+from src.utils.logger import setup_logger
 
 class HelpCog(commands.Cog):
     def __init__(self, bot: DiscordBot):
         self._bot = bot
+        self._logger = setup_logger('bot.cog.help', '/data/discord.log')
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self._logger.info("help cog is ready.")
 
     @commands.hybrid_command(name="help")
     async def help(self, ctx: commands.context.Context):
@@ -16,13 +22,18 @@ class HelpCog(commands.Cog):
             **/event** - Displays current events.
             **/event upcoming** - Displays upcoming events.
             **/event ending** - Displays events ending.
-            **/reminder** - Add a new reminder
+            **/reminder** - Add a new reminder.
             **/reminder delete** - Delete all your reminders.
+            
+            __**Misc**__
             **/help** - Gets a list of commands.
+            **/coinflip** - Flip a coin.
+            **/random** - Chooses a number between 1 to X.
+            **/shoud** - Ask a question, bot will give a vague answer. 
 
             __**Setting Commands**__
-            **/setup** - Setup notification and ping role server settings
-            **/setup delete** - Delete server settings
+            **/setup** - Setup notification and ping role server settings.
+            **/setup delete** - Delete server notification settings.
             """)
         embed = discord.embeds.Embed(title="Command List", description=message, color=discord.Colour.blurple())
 
