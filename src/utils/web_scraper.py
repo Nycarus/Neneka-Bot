166a103e16aa5c756acb34b0b3b2cbf15b2cc43d
Anthony Huang <Huang.Anthony@outlook.com>
2023-01-05 15:15:29 -0500

    Update web_scraper.py

--- src/utils/web_scraper.py
+++ src/utils/web_scraper.py
@@ -27,51 +27,60 @@ class WebScraper:
         
         # Iterate through event and create dictionary
         for li in content[-1].find_all('li', recursive=False):
-            event = li.find(text = True).strip()
-            
-            # Find opening bracket for date
-            startingBracket = -1
-            for i in range(len(event)-1,-1,-1):
-                if (event[i] == "("):
-                    startingBracket = i
-                    break
-            
-            # Split crunchyroll event string into 2 parts
-            eventName, date = event[0:startingBracket].lstrip().rstrip(), event[startingBracket:].strip("(").strip(")").split()
-            
-            # Add seconds to date
-            if (date[1].count(":")==1):
-                date[1] += ":00"
-
-            # Add current year to date if it does not exist, because it defaults to 1900
-            if (date[0].count('/') == 1):
-                date[0] += "/" + str(datetime.now().year)
-
-            startDateString = " ".join(date[0:2])
-
-            # Convert date string to datetime format
-            startDate = datetime.strptime(startDateString, '%m/%d/%Y %H:%M:%S')
-
-
-            # Check if there's an end date
-            if (len(date) > 5):
+            try:
+                event = li.find(text = True).strip()
                 
+                # Find opening bracket for date
+                startingBracket = -1
+                for i in range(len(event)-1,-1,-1):
+                    if (event[i] == "("):
+                        startingBracket = i
+                        break
+                
+                # Split crunchyroll event string into 2 parts
+                eventName, date = event[0:startingBracket].lstrip().rstrip(), event[startingBracket:].strip("(").strip(")").split()
+                
+                # Check if it's a proper date: (MM/DD/YYYY TIME UTC) or (MM/DD TIME UTC)
+                if (date and (date[0].count("/") < 1 or len(date) < 3 or date[0].count("/") > 2)):
+                    logger.info("Possible new date or web format.")
+                    continue
+
                 # Add seconds to date
-                if (date[-2].count(":")==1):
-                    date[-2] += ":00"
+                if (date[1].count(":")==1):
+                    date[1] += ":00"
 
                 # Add current year to date if it does not exist, because it defaults to 1900
-                if (date[-3].count("/")==1):
-                    date[-3] += "/" + str(datetime.now().year)
-                
-                endDateString = " ".join(date[4:-1])
+                if (date[0].count('/') == 1):
+                    date[0] += "/" + str(datetime.now().year)
+
+                startDateString = " ".join(date[0:2])
 
                 # Convert date string to datetime format
-                endDate = datetime.strptime(endDateString, '%m/%d/%Y %H:%M:%S')
-            else:
-                endDate = None
+                startDate = datetime.strptime(startDateString, '%m/%d/%Y %H:%M:%S')
 
-            eventDetails = {"event": eventName, "startDate": startDate, "endDate": endDate}
-            events.append(eventDetails)
+
+                # Check if there's an end date
+                if (len(date) == 7 and date[0].count("/") >= 1 and date[-3].count("/") >= 1):
+                    
+                    # Add seconds to date
+                    if (date[-2].count(":")==1):
+                        date[-2] += ":00"
+
+                    # Add current year to date if it does not exist, because it defaults to 1900
+                    if (date[-3].count("/")==1):
+                        date[-3] += "/" + str(datetime.now().year)
+                    
+                    endDateString = " ".join(date[4:-1])
+
+                    # Convert date string to datetime format
+                    endDate = datetime.strptime(endDateString, '%m/%d/%Y %H:%M:%S')
+                else:
+                    endDate = None
+
+                eventDetails = {"event": eventName, "startDate": startDate, "endDate": endDate}
+                events.append(eventDetails)
+            except Exception as e:
+                logger.error(e)
+                logger.error("Something went wrong with creating event. Possible new web format.")
         
         return events
